305c3ac30286e7b3b1bd82d586efb715314884bd
LuisDio <luis.diolindo@gmail.com>
2018-10-29 21:52:07 +0300

    Add convolutional layer

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -22,7 +22,16 @@ y_test = keras.utils.to_categorical(y_test, 10)
 
 # Create a model and add layer
 model = Sequential()
-model.add(Dense(512, activation="relu", input_shape=(32, 32, 3)))
+
+model.add(Conv2D(32, (3, 3), padding="same", activation="relu", input_shape=(32, 32, 3)))
+model.add(Conv2D(32, (3, 3), activation="relu"))
+
+model.add(Conv2D(64, (3, 3), padding="same", activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+
+model.add(Flatten())
+
+model.add(Dense(512, activation="relu"))
 model.add(Dense(10, activation="softmax"))
 
 # Print a summary of the object
